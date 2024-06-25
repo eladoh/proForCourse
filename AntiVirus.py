8d9f776a1a5a7cf60e1ab54ca67abb9cb51d447b
eladoh <eladoh244@gmail.com>
2024-06-25 23:39:13 +0300

    update

--- AntiVirus.py
+++ AntiVirus.py
@@ -40,9 +40,39 @@ def type(words: str):
         sys.stdout.flush()
     print()
 
+#https://virustotal.readme.io/v2.0/reference/file-scan
 def check_for_virus():
-    
-print(check_for_virus())
+
+    url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+
+    params = {'apikey': 'https://www.virustotal.com/vtapi/v2/file/scan/upload_url'}
+
+    files = {'file': ('myfile.exe', open('C:/Users/user1/Desktop/corcu/secret_message.jpg', 'rb'))}
+
+    response = requests.post(url, files=files, params=params)
+
+    print(response.json())
+
+    # url = "https://www.virustotal.com/vtapi/v2/file/scan/upload_url"
+
+    # params = {'apikey':'f205ae5df052b585445febb5df5ea7c7c41bfd13bba3eaf348cf59718022327d'}
+
+    # response = requests.get(url, params=params)
+
+    # print(response.json())
+    # upload_url_json = response.json()
+    # upload_url = upload_url_json['upload_url']
+
+    # file_to_scan= r"C:/Users/user1/Desktop/corcu/secret_message.jpg"
+    # files = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
+    # response = requests.post(upload_url, files=files)
+
+    # print(response.text)
+
+
+
+
+check_for_virus()
 
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,15 @@
-pandas.read_csv(
+    # url = "https://www.virustotal.com/vtapi/v2/file/scan/upload_url"
+
+    # params = {'apikey':'f205ae5df052b585445febb5df5ea7c7c41bfd13bba3eaf348cf59718022327d'}
+
+    # response = requests.get(url, params=params)
+
+    # print(response.json())
+    # upload_url_json = response.json()
+    # upload_url = upload_url_json['upload_url']
+
+    # file_to_scan= r"C:/Users/user1/Desktop/corcu/secret_message.jpg"
+    # files = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
+    # response = requests.post(upload_url, files=files)
+
+    # print(response.text)
