2d638b09d2d99289d6325cd027144ef6031b8541
eladoh <eladoh244@gmail.com>
2024-06-29 22:13:29 +0300

    5ery4t

--- hida_svoit7.py
+++ hida_svoit7.py
@@ -1,20 +1,22 @@
 
 
-arr = [1,2 ,3 ,4, -1 ,-2 ,-3 ,-4,]
+arr = [1,2 ,3 ,4, -10 ,9 ,10 ,-4,]
 
-def higest_sequence(arr, sequence_length):
+def higest_sequence(arr):
     
-    higest_sequence_sum = 0
-    sequence_sum = 0
+    higest_sequence_sum = arr[0]
+    sequence_sum = arr[0]
 
     for i in range(0, len(arr)):
-        sequence_sum = sum(arr[i:i + sequence_length])
-        if sequence_sum > higest_sequence_sum:
-            higest_sequence_sum = sequence_sum
+        for x in range(0, len(arr)):
+            sequence_sum = sum(arr[i:x])
+
+            if sequence_sum > higest_sequence_sum:
+                higest_sequence_sum = sequence_sum
 
     return higest_sequence_sum
 
-print(higest_sequence(arr, 2))
+print(higest_sequence(arr))
         
         
         
