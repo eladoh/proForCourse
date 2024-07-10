28657cb5822eadbd978b779ca82687a23db8c892
eladoh <eladoh244@gmail.com>
2024-07-10 22:59:01 +0300

    adding funcitons

--- server-client/server.py
+++ server-client/server.py
@@ -1,43 +1,59 @@
 import socket
 import os
+import threading
 
 
-host = '127.0.0.1'
-port = 8080
+def handle_client():
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
-sock.listen()
+def main():
 
-print(f'Server listening on {host}:{port}...')
+    host = '127.0.0.1'
+    port = 8080
 
-conn, addr = sock.accept()
-print(f'Connected with client {addr}')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen()
+    print(f'Server listening on {host}:{port}...')
 
-# Receive the length of the filename
-filename_length = int(conn.recv(8).decode())
-# Receive the filename itself
-filename = conn.recv(filename_length).decode()
+    while True:
+        conn, addr = sock.accept()
+        print(f'Connected with client {addr}')
 
-print(f'Received filename: {filename}')
-file_basename = os.path.basename(filename)
-file_path = os.path.join("C:\\Users\\user1\\Desktop\\proForCourse\\files", file_basename)
+        upload_or_download = conn.recv(1024).decode()
 
-# Receive the length of the data
-data_length = int(conn.recv(16).decode())
-print(f'Expecting to receive {data_length} bytes')
+        if(upload_or_download == "upload"):
+            print("upload")
+        if upload_or_download == "download":
+            print("download")
 
-with open(file_path, "wb") as fo:
-    received_data = 0
-    while received_data < data_length:
-        data = conn.recv(1024)
-        if not data:
-            break
-        fo.write(data)
-        received_data += len(data)
+        # client_thread = threading.Thread(target=handle_client, args=(conn, addr, command_received))
+        # client_thread.start()
 
-print(f'Received {received_data} bytes')
-conn.close()
-sock.close()
-print(f'File saved to {file_path}')
-dadad
+# # Receive the length of the filename
+# filename_length = int(conn.recv(8).decode())
+# # Receive the filename itself
+# filename = conn.recv(filename_length).decode()
+
+# print(f'Received filename: {filename}')
+# file_basename = os.path.basename(filename)
+# file_path = os.path.join("C:\\Users\\user1\\Desktop\\proForCourse\\files", file_basename)
+
+# # Receive the length of the data
+# data_length = int(conn.recv(16).decode())
+# print(f'Expecting to receive {data_length} bytes')
+
+# with open(file_path, "wb") as fo:
+#     received_data = 0
+#     while received_data < data_length:
+#         data = conn.recv(1024)
+#         if not data:
+#             break
+#         fo.write(data)
+#         received_data += len(data)
+
+# print(f'Received {received_data} bytes')
+# conn.close()
+# sock.close()
+# print(f'File saved to {file_path}')
+
+main()
